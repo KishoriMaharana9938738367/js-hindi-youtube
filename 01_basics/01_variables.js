8737c8546cc178e2f1d57f4a0b36609aeb7a1c38
KishoriMaharana9938738367 <kishorimaharana63@gmail.com>
2024-06-12 08:58:17 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 19012002
+let accountEmail = " kishori@google.com"
+var acccountpassword = "12345"
+accountCity = "Berhampur"
+
+// accountId =20082002 // not allowed
+
+accountEmail ="kis01@google.com"
+acccountpassword ="34567"
+accountCity ="jaopur"
+
+console.log(accountId);
+
+console.table([accountId,acccountpassword,accountEmail,accountCity])
