81df108ad4bb85647917a4f49a015d7e174fe605
KishoriMaharana9938738367 <kishorimaharana63@gmail.com>
2024-07-15 07:56:23 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,16 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
  let stringNumber = String(someNumber)
  console.log(stringNumber);
  console.log(typeof stringNumber); 
+
+ // **************** Operations *******************
+
+ let value = 3
+ let negValue = -value
+ console.log(negValue);
+
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**3);
+ console.log(2/3);
+ console.log(2%3);
